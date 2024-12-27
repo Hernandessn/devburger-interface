6d0361aa33920b3daa26328661f6b0de54073114
Hernandessn <hernandessalesneves7@gmail.com>
2024-12-27 14:34:59 -0300

    Adicionado formulário com React Hook form

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,3 +1,7 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import {
 	Container,
 	Form,
@@ -11,6 +15,22 @@ import Logo from "../../assets/logo.png";
 import { Button } from "../../components/Button";
 
 export function Login() {
+	const schema = yup
+		.object({
+			email: yup.string().email().required(),
+			password: yup.string().min(6).required(),
+		})
+		.required();
+	
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
+	const onSubmit = (data) => console.log(data);
+
 	return (
 		<Container>
 			<LeftContainer>
@@ -18,25 +38,29 @@ export function Login() {
 			</LeftContainer>
 			<RightContainer>
 				<Title>
-					Olá, seja bem vindo ao <span>Dev Burger!</span> 
-                    <br />
-                    Acesse com seu <span>Login e senha.</span>
+					Olá, seja bem vindo ao <span>Dev Burger!</span>
+					<br />
+					Acesse com seu <span>Login e senha.</span>
 				</Title>
-				<Form>
+				<Form onSubmit={handleSubmit(onSubmit)}>
 					<InputContainer>
+						{/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
 						<label>Email</label>
-						<input type="email" />
+						<input type="email" {...register("email")} />
 					</InputContainer>
 
 					<InputContainer>
+						{/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
 						<label>Senha</label>
-						<input type="password" />
+						<input type="password" {...register("password")} />
 					</InputContainer>
-					<Button>Entrar</Button>
+					<Button type="submit">Entrar</Button>
 				</Form>
 				<p>
-                    Não possui conta? <a>Clique aqui.</a>
-                 </p>
+					Não possui conta?
+					{/* biome-ignore lint/a11y/useValidAnchor: <explanation> */}
+					<a>Clique aqui.</a>
+				</p>
 			</RightContainer>
 		</Container>
 	);
