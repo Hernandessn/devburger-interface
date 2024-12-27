12fec752cc3a5d11619bf2af7287af5ee3b1006f
Hernandessn <hernandessalesneves7@gmail.com>
2024-12-27 14:06:46 -0300

    Commit inicado

---
+++ src/containers/Login/index.jsx
@@ -0,0 +1,43 @@
+import {
+	Container,
+	Form,
+	InputContainer,
+	LeftContainer,
+	RightContainer,
+	Title,
+} from "./styles";
+
+import Logo from "../../assets/logo.png";
+import { Button } from "../../components/Button";
+
+export function Login() {
+	return (
+		<Container>
+			<LeftContainer>
+				<img src={Logo} alt="logo-devburger" />
+			</LeftContainer>
+			<RightContainer>
+				<Title>
+					Olá, seja bem vindo ao <span>Dev Burger!</span> 
+                    <br />
+                    Acesse com seu <span>Login e senha.</span>
+				</Title>
+				<Form>
+					<InputContainer>
+						<label>Email</label>
+						<input type="email" />
+					</InputContainer>
+
+					<InputContainer>
+						<label>Senha</label>
+						<input type="password" />
+					</InputContainer>
+					<Button>Entrar</Button>
+				</Form>
+				<p>
+                    Não possui conta? <a>Clique aqui.</a>
+                 </p>
+			</RightContainer>
+		</Container>
+	);
+}
