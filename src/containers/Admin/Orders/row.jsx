34bc1404fd43c4fa6e51c5bb5f218a140669ca70
Hernandessn <hernandessalesneves7@gmail.com>
2025-01-16 10:50:41 -0300

    Adicionando a funcionalidades na tabela: Implemetação do React Select e na criação para Formatação de Datas

--- src/containers/Admin/Orders/row.jsx
+++ src/containers/Admin/Orders/row.jsx
@@ -10,14 +10,23 @@ import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';;
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { Fragment, useState } from 'react';
 
+
+import { Fragment, useState } from 'react';
+import { formatDate } from '../../../utils/formatDate';
+import { ProductImage, SelectStatus } from './styles';
+import { orderStatusOptions } from './orderStatus';
+import { api } from '../../../services/api';
 
 
 
 export function Row(props) {
     const { row } = props;
     const [open, setOpen] = useState(false);
+
+    async function newStatusOrder() {
+      await api.put(`orders/${id}`, { status })
+    }
   
     return (
       <Fragment>
@@ -34,9 +43,18 @@ export function Row(props) {
           <TableCell component="th" scope="row">
             {row.orderId}
           </TableCell>
-          <TableCell align="right">{row.name}</TableCell>
-         <TableCell align="right">{row.date}</TableCell>
-          <TableCell align="right">{row.status}</TableCell>
+          <TableCell >{row.name}</TableCell>
+         <TableCell >{formatDate(row.date) }</TableCell>
+          <TableCell >
+            <SelectStatus 
+            options={orderStatusOptions.filter((status) => status.id !== 0)}
+            placeholder="Status"
+            defaultValue={ orderStatusOptions.find( 
+              status => status.value === row.status || null,
+            )}
+            onChange={ status => newStatusOrder(row.orderId, status.value)}
+            />
+          </TableCell>
         </TableRow>
         <TableRow>
           <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -51,7 +69,7 @@ export function Row(props) {
                       <TableCell>Quantidade</TableCell>
                       <TableCell>Produto</TableCell>
                       <TableCell >Categoria</TableCell>
-                      <TableCell > </TableCell>
+                      <TableCell >Imagem do Produto </TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
@@ -61,9 +79,9 @@ export function Row(props) {
                         {product.id}
                         </TableCell>
                         <TableCell>{product.name}</TableCell>
-                        <TableCell align="right">{product.category}</TableCell>
+                        <TableCell>{product.category}</TableCell>
                         <TableCell>
-                         <img src={product.url}  alt={product.name}/>
+                         <ProductImage src={product.url}  alt={product.name}/>
                         </TableCell>
                       </TableRow>
                     ))}
